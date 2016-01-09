147c5a7e21ed54f6a652ae370a25d7fb7847c30b
Dinuka <dinuka.13@cse.mrt.ac.lk>
2016-01-10 04:39:46 +0530

    Display Code Mostly Done

--- dsplay_code.cpp
+++ dsplay_code.cpp
@@ -24,56 +24,14 @@ void draw(void) {
     break;
 
     case 1:
-      
       u8g.drawFrame(110,0,15,7);
       u8g.drawBox(117,0,7,7);
       u8g.drawStr( 20, 10, "JoggerMate");
-
+      u8g.drawTriangle();
       break;
       
     case 2:
-      u8g.drawFrame(110,0,15,7);
-      u8g.drawBox(117,0,7,7);
-      u8g.drawStr( 20, 10, "JoggerMate");
-      //left arrow
-      u8g.drawTriangle(0,35,5,40,5,30);
-      u8g.drawBox(5,33,8,4);
-      //right arrow
-      u8g.drawTriangle(45,35,39,40,39,30);
-      u8g.drawBox(31,33,8,4);
-      
-      u8g.drawStr( 5, 55, "1");
-      u8g.drawStr( 30, 55, "2");
-
-      //progress bars
-      u8g.drawStr( 57, 35, "1 ||||||||||");
-      u8g.drawStr( 57, 55, "2 |||");
     break;
-
-    case 3:
-      u8g.drawFrame(110,0,15,7);
-      u8g.drawBox(117,0,7,7);
-      u8g.drawStr( 20, 10, "JoggerMate");
-      u8g.setFont(u8g_font_profont11);
-
-      // left arrow
-      u8g.drawTriangle(0,50,5,55,5,45);
-      u8g.drawBox(5,48,8,4);
-      // right arrow
-      u8g.drawTriangle(45,50,39,55,39,45);
-      u8g.drawBox(31,48,8,4);
-      // up arrow
-      u8g.drawTriangle(22,23,17,30,27,30); 
-      u8g.drawBox(20,30,4,8);
-      u8g.drawStr( 9, 34, "1");
-      u8g.drawStr( 5, 64, "2");
-      u8g.drawStr( 30, 64, "3");
-
-      //progress bars
-      u8g.drawStr( 57, 30, "1 |||||");
-      u8g.drawStr( 57, 45, "2 ||||||||||");
-      u8g.drawStr( 57, 60, "3 |||");
-      break;
   }
 }
 
@@ -103,15 +61,8 @@ void loop() {
       draw();
     } while( u8g.nextPage() );
     if((millis()-t)>1500){
-      t=millis();
-      if(drawState==2){
-        drawState=3;  
-      }else{
-        drawState=2;
-      }
-      
+      drawState=1;
     }
   }
 }
-
 
